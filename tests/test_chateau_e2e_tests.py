634eedffe1540fd67a07f4bbd1c24a971e326dbb
Thomas Leese <thomas@leese.io>
2020-11-14 11:07:29 +0000

    Move configuration into conftest.py

--- tests/test_chateau_e2e_tests.py
+++ tests/test_chateau_e2e_tests.py
@@ -4,27 +4,6 @@ import uuid
 import pytest
 
 
-@pytest.fixture
-def selenium(selenium):
-    selenium.implicitly_wait(10)
-    return selenium
-
-
-@pytest.fixture
-def chrome_options(chrome_options):
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    return chrome_options
-
-
-@pytest.fixture
-def firefox_options(firefox_options):
-    firefox_options.add_argument("--headless")
-    return firefox_options
-
-
 @pytest.mark.nondestructive
 def test_homepage(selenium, base_url):
     selenium.get(base_url)
