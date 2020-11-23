7167c265ad610402b5182bacbe92a7c8af70acbd
Thomas Leese <thomas@leese.io>
2020-11-23 19:46:31 +0000

    Add a test for uploading an organisation logo
    Because it covers lots of services.

--- tests/test_organisation_settings.py
+++ tests/test_organisation_settings.py
@@ -1,4 +1,23 @@
+from pathlib import Path
+
 from . import helpers
+
+
+def test_change_logo(selenium, organisation_name):
+    helpers.create_organisation_and_sign_in(selenium, name=organisation_name)
+
+    helpers.click_link(selenium, text="Settings")
+
+    helpers.fill_form(
+        selenium,
+        file=str(Path(__file__).parent.parent / "fixtures" / "logo.png"),
+    )
+
+    selenium.find_element_by_xpath('//button[text()="Upload"]').click()
+
+    selenium.find_element_by_xpath('//*[text()="Success!"]')
+
+    selenium.find_element_by_xpath('//*[@class="navbar-brand"]//img')
 
 
 def test_payment_subscribe(selenium, organisation_name):
